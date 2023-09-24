c3d9599aa4ab4eab03ac944d9b75bfec6ae6174e
himanganimesh2 <himanganimesh2@gmail.com>
2023-09-24 16:15:42 +0530

    station login added

--- src/pages/login/UserLogin.js
+++ src/pages/login/UserLogin.js
@@ -6,8 +6,9 @@ import { Link } from "react-router-dom";
 export const UserLogin = () => {
     const [email, setEmail] =useState("");
     const [password, setPassword] = useState("");
+    const [userLogin, setUserLogin] = useState(true);
 
-    const handleLogin = async(e) => {
+    const handleUserLogin = async(e) => {
         e.preventDefault();
         try {
             const res = await axios.post("auth/user/signin", {email, password})
@@ -16,34 +17,73 @@ export const UserLogin = () => {
             console.log(error);
         }
     }
+    const handleStationLogin = async(e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.post("auth/station/signin", {email, password})
+            console.log(res.data)
+        } catch (error) {
+            console.log(error);
+        }
+    }
     return (
         <div className="UserLoginWrapper">
-            <div>
-                <button></button>
-            </div>
-            <div className="card">
-                <h1>Login as a User</h1>
-                <input 
-                    type="text"
-                    placeholder="Email"
-                    value={email}
-                    onChange={e=> {
-                        setEmail(e.target.value)
-                    }}
-                />
-                <input 
-                    type="password"
-                    placeholder="password"
-                    value={password}
-                    onChange={e=> {
-                        setPassword(e.target.value)
-                    }}
-                />
-                <button onClick={handleLogin}>Login</button>
-                <div className="reg">
-                    <Link to="/register">Register</Link> 
+            <div className="topSection">
+                <p>Chose a way u like to Login</p>
+                <div className="buttons">
+                    <button onClick={() => setUserLogin(true)}>Login as a User</button>
+                    <button onClick={() => setUserLogin(false)}>Login as a Station</button>
                 </div>
             </div>
+                {userLogin ? (
+                    <div className="card">
+                        <h1>Login as a User</h1>
+                        <input 
+                            type="text"
+                            placeholder="Email"
+                            value={email}
+                            onChange={e=> {
+                                setEmail(e.target.value)
+                            }}
+                        />
+                        <input 
+                            type="password"
+                            placeholder="password"
+                            value={password}
+                            onChange={e=> {
+                                setPassword(e.target.value)
+                            }}
+                        />
+                        <button onClick={handleUserLogin}>Login</button>
+                        <div className="reg">
+                            <Link to="/register">Register</Link> 
+                        </div>
+                    </div>
+                ) : (
+                    <div className="card">
+                        <h1>Login as a Station</h1>
+                        <input 
+                            type="text"
+                            placeholder="Email"
+                            value={email}
+                            onChange={e=> {
+                                setEmail(e.target.value)
+                            }}
+                        />
+                        <input 
+                            type="password"
+                            placeholder="password"
+                            value={password}
+                            onChange={e=> {
+                                setPassword(e.target.value)
+                            }}
+                        />
+                        <button onClick={handleStationLogin}>Login</button>
+                        <div className="reg">
+                            <Link className="link" to="/register">Register</Link> 
+                        </div>
+                    </div>
+                )}
         </div>
     )
 }
